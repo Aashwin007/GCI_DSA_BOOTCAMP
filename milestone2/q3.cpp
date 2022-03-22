8fef5e71cb70a10dd8c1878bf09a098d3284640a
Aashwin007 <101468031+Aashwin007@users.noreply.github.com>
2022-03-22 19:51:36 +0530

    Add files via upload

---
+++ milestone2/q3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    char n = 'a';
+    while (n <= 'z')
+    {
+        cout << n << endl;
+        n++;
+    }
+
+    return 0;
+}
---
+++ milestone2/q7.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n, sum = 0;
+    cin >> n;
+    for (int i = 0; i <= n; i += 2)
+    {
+        sum += i;
+    }
+    cout << sum << endl;
+
+    return 0;
+}
